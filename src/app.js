a724b4dd459f5666a0f0e1895d1815a672002ddf
anarg1812 <147712294+anarg1812@users.noreply.github.com>
2024-01-10 10:08:41 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generadorDeExcusas();
 };
+function generadorDeExcusas() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoAleatorio = Math.floor(Math.random() * who.length);
+  let actionAleatorio = Math.floor(Math.random() * action.length);
+  let whatAleatorio = Math.floor(Math.random() * what.length);
+  let whenAleatorio = Math.floor(Math.random() * when.length);
+  return (
+    who[whoAleatorio] +
+    " " +
+    action[actionAleatorio] +
+    " " +
+    what[whatAleatorio] +
+    " " +
+    when[whenAleatorio]
+  );
+}
